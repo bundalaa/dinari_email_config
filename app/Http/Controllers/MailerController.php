dc12e8b01c75db5d7e64ae44a91eacec0f19b959
Elizabeth <betyevaristy@gmail.com>
2022-11-03 12:47:33 +0300

    initial changes

---
+++ app/Http/Controllers/MailerController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\VerifyMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class MailerController extends Controller
+{
+        ////////////send email////////////
+        public function sendEmail(Request $request,$email)
+        {
+            $rules = [
+                'email' => 'required|string|unique:users',
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+            $pin = $this->generatePin();    
+            $otp = $this->generateOtp();  
+                 
+            $response = $codeVerification = ([
+                'otp' => $otp,
+                'pin' => $pin
+            ]);
+            Mail::to($email)->send(new VerifyMail($codeVerification));
+            return response()->json($response, 201);
+        }
+    
+            ////////generate pin//////////
+    public function generatePin() {
+        $pin = '';
+        for ($i = 0; $i < 4; $i++) {
+            $pin = $pin . mt_rand(0, 9);
+        }
+    }
+    
+        ////////generate otp//////////
+        public function generateOtp() {
+            $otp = '';
+            for ($i = 0; $i < 5; $i++) {
+                $otp = $otp . mt_rand(0, 9);
+            }
+        }
+}
